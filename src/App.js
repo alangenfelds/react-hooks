4b172495b0f0e3cb44396840148c67e315c7df40
Artur Lan <alangenfeld@gmail.com>
2019-05-03 15:10:16 +0300

    added geolocation events

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React, { useState, useEffect } from "react";
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
+
 const App = () => {
   // [stateParameter, parameterModifyerFunction] = initial value
   const [count, setCount] = useState(0);
   const [isOn, toggle] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(
+    initialLocationState
+  );
+
+  let mounted = true;
 
   useEffect(() => {
     // executes at every render
     document.title = `You have clicked ${count} times`;
     window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    navigator.geolocation.getCurrentPosition(handleGeolocation);
+    const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
     // executes on unmount - to cleanup
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+      mounted = false;
+      navigator.geolocation.clearWatch(watchId); // cleanup listener
     };
-  }, []);
+  }, [count, status]); // when this second parameter is added to useEffect - eventlisteners will only run on mount and unmount
+
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
+
+  const handleOnline = () => {
+    setStatus(true);
+  };
+
+  const handleOffline = () => {
+    setStatus(false);
+  };
 
   const handleMouseMove = ({ pageX, pageY }) => {
     setMousePosition({ x: pageX, y: pageY });
@@ -51,6 +89,14 @@ const App = () => {
       <h2>Mouse position</h2>
       {JSON.stringify(mousePosition, null, 2)}
       <br />
+      <h2>Network Status:</h2>
+      <p>
+        You are <strong>{status ? "ONLINE" : "OFFLINE"}</strong>
+      </p>
+      <h2>Geolocation:</h2>
+      <p>Latitude is {latitude}</p>
+      <p>Lomngitude is {longitude}</p>
+      <p>Your speed is {speed ? speed : 0}</p>
     </>
   );
 };
