9b2fca02adcacc115f880e51e97b6d5b8899d4cb
Artur Lan <alangenfeld@gmail.com>
2019-05-03 14:00:54 +0300

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  // [stateParameter, parameterModifyerFunction] = initial value
+  const [count, setCount] = useState(0);
+  const [isOn, toggle] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+  useEffect(() => {
+    // executes at every render
+    document.title = `You have clicked ${count} times`;
+    window.addEventListener("mousemove", handleMouseMove);
+
+    // executes on unmount - to cleanup
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const handleMouseMove = ({ pageX, pageY }) => {
+    setMousePosition({ x: pageX, y: pageY });
+  };
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const toggleLight = () => {
+    toggle(prevState => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>I was clicked {count} times</button>
+      <h2>Toggle Light</h2>
+      <img
+        src={
+          isOn
+            ? "https://icon.now.sh/highlight/fd0"
+            : "https://icon.now.sh/highlight/aaa"
+        }
+        style={{
+          height: "50px",
+          width: "50px"
+          // background: isOn ? "yellow" : "gray"
+        }}
+        alt="Flashlight"
+        onClick={toggleLight}
+      />
+      <h2>Mouse position</h2>
+      {JSON.stringify(mousePosition, null, 2)}
+      <br />
+    </>
   );
-}
+};
 
 export default App;
